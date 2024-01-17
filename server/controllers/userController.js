e0c10d4f1348ccd9fe72b89557f69d7b3703f41d
Douglas Maalongaye <douglastawile@gmail.com>
2024-01-17 21:11:30 +0000

    Finished with the user profile api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,3 +24,13 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const user = req.profile;
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUsers, userByID } from "../controllers/userController.js";
+import { getUser, getUsers, userByID } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/users").get(getUsers);
+
+router.route("/users/:userId").get(getUser);
 
 router.param("userId", userByID);
 
