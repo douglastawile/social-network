3eb982ea265d94c43927d095c588f6ce61c0d677
Douglas Maalongaye <douglastawile@gmail.com>
2024-01-17 21:31:26 +0000

    Finished with the update user profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { User } from "../models/UserModel.js";
 
+//Getting user by ID
 export const userByID = async (req, res, next, id) => {
   try {
     const user = await User.findById(id);
@@ -15,6 +16,7 @@ export const userByID = async (req, res, next, id) => {
   }
 };
 
+// Feching all users from the database
 export const getUsers = async (req, res) => {
   try {
     const users = await User.find();
@@ -25,10 +27,46 @@ export const getUsers = async (req, res) => {
   }
 };
 
+// Get a single user from the database
 export const getUser = async (req, res) => {
   try {
     const user = req.profile;
     return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+// Update and save user info to the database
+const filteredObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    if (req.body.password || req.body.passwordConfirm) {
+      return res
+        .status(422)
+        .json({ error: "This route is not for password update." });
+    }
+
+    const filteredBody = filteredObj(req.body, "name", "email");
+    const updatedUser = await User.findByIdAndUpdate(
+      req.profile._id,
+      filteredBody,
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).json({
+      status: "Success",
+      message: "User account updated successfully",
+      user: updatedUser,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Server error" });
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { getUser, getUsers, userByID } from "../controllers/userController.js";
+import {
+  getUser,
+  getUsers,
+  updateUser,
+  userByID,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/users").get(getUsers);
 
-router.route("/users/:userId").get(getUser);
+router.route("/users/:userId").get(getUser).patch(updateUser);
 
 router.param("userId", userByID);
 
